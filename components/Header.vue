66fb581242b8597c7d32f59c3a61fad6e93275e8
Brock Herion <brockheriondev@gmail.com>
2021-09-05 05:54:02 -0500

    Added nav links

--- components/Header.vue
+++ components/Header.vue
@@ -1,19 +1,35 @@
 <template>
-  <header class="px-14 py-7">
-    <span>
+  <header class="mx-80 my-7">
+    <div class="flex items-center justify-between">
       <a class="flex text-lg items-center" href="/">
         <terminal-icon />
         Brock Herion
       </a>
-    </span>
+      <ul class="flex items-center">
+        <li class="mr-6">
+          <a href="/articles">Articles</a>
+        </li>
+        <li class="mr-6">
+          <a href="/about">About</a>
+        </li>
+        <li class="mr-6">
+          <a href="/tech">Tech</a>
+        </li>
+        <li class="mr-6">
+          <a href="/newsletter">Newsletter</a>
+        </li>
+      </ul>
+    </div>
   </header>
 </template>
 <script lang="ts">
-import { TerminalIcon } from 'vue-feather-icons';
+import {
+  TerminalIcon,
+} from 'vue-feather-icons';
 
 export default {
   components: {
-    TerminalIcon
+    TerminalIcon,
   }
 }
 </script>
