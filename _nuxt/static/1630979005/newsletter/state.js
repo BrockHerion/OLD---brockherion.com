add2709e36497925584badf1513ca3e75fc801f2
BrockHerion <BrockHerion@users.noreply.github.com>
2021-09-07 01:43:27 +0000

    deploy: 5f480c2f0ae8231470912fdb9bf7bf4d689f47aa

---
+++ _nuxt/static/1630979005/newsletter/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fbrockherion.com\u002F_nuxt\u002Fstatic\u002F1630979005",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{_app:{basePath:"\u002Fbrockherion.com\u002F",assetsPath:"\u002Fbrockherion.com\u002F_nuxt\u002F",cdnURL:null}}}}("\u002Fnewsletter"));
