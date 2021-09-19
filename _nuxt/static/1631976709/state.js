022b1468f2113bb5781d9ecbf367bb5c6dfd7307
BrockHerion <BrockHerion@users.noreply.github.com>
2021-09-19 14:19:02 +0000

    deploy: cc37e3e12c5adbe31e67c5a0743bf679fead66b7

--- _nuxt/static/1631976709/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fbrockherion.com\u002F_nuxt\u002Fstatic\u002F1631976709",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fbrockherion.com\u002F",assetsPath:"\u002Fbrockherion.com\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ba97f4c8"}}}}(null));
